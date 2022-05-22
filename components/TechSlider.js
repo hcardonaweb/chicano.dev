b9ffee9083a2ec041f913d1d16d8d57b138786c1
Hector Cardona <hector@tm5150.com>
2022-05-22 09:22:12 -0500

    added swiper js and tech carousel

---
+++ components/TechSlider.js
@@ -0,0 +1,46 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+
+import TechData from "../data/TechData.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const TechSlider = () => {
+  SwiperCore.use([Autoplay]);
+  return (
+    <section>
+      <Swiper
+        // install Swiper modules
+        modules={[Autoplay]}
+        spaceBetween={25}
+        slidesPerView={5}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+      >
+        {TechData.map((tech) => (
+          <>
+            <SwiperSlide key={tech.id}>
+              <FontAwesomeIcon
+                className="px-1 m-auto techIcon h-7"
+                icon={["fab", `${tech.icon}`]}
+              />
+          
+            </SwiperSlide>
+          </>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default TechSlider;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Technologies from "../components/Technologies";
 import "../src/fontawesome/fontawesome";
+import TechSlider from "../components/TechSlider";
 
 export default function Home() {
   return (
@@ -24,24 +25,18 @@ export default function Home() {
         <p className="text-xl">💻 🎧 ☕️</p>
       </div>
       <div className="my-10">
-        <div className="grid-gap-2 my-2 grid md:grid-cols-2">
+        <div className="grid-gap-2 my-2">
           <div className="mb-8">
-            <p className="mb-4 text-md">
-              A <i>front-end web developer</i> focused on Html, Css &amp;
-              JavaScript. By day I am a Web Developer at TM5150. By night I am
-              just like you, trying to get better at my craft one line of code
-              at a time. If I’m not reading a book/manga then I’m probably
-              binging a Netflix series whilst <i>slaying</i> some tacos. I
-              occasionally write on my blog about what I’m learning or my
-              current technical roadblock. If you feel like following a normal
+            <p className="text-md mb-4">
+              A <i>front-end web developer</i> focused on Html, CSS &amp;
+              JavaScript. By day I am a Front-End Web Developer. By night I am 
+              working on my never ending list of side projects. If you feel like following a normal
               person on Twitter, click here. No threads, pinky promise.
             </p>
             <p className="text-sm">- Your Friendly Neighborhood Chicano</p>
           </div>
-          <div>
-            <Technologies />
-          </div>
-        </div>
+        </div>      
+        <TechSlider />
       </div>
     </div>
   );
