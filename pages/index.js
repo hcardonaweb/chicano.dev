85fc1a945f6f2dfab4d2c8c9939c7aebd943d189
Hector Cardona <hector@tm5150.com>
2022-10-17 16:17:37 -0700

    clean slate

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,21 @@
-import Head from "next/head";
-import "../src/fontawesome/fontawesome";
-import TechSlider from "../components/TechSlider";
-import FeaturedProjects from "../components/FeaturedProjects";
+import Head from 'next/head'
+import Hero from "../components/Hero";
+
+import 'dracula-ui/styles/dracula-ui.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className="drac-black-secondary"> {/*className={styles.container} */}
       <Head>
         <title>Hector Cardona - Front-End Web Developer</title>
         <meta
           name="description"
           content="Hector Cardona - Front-End Web Developer"
         />
-        <link rel="icon" href="/favicon.png" />
-        <link
-          rel="preload"
-          href="/public/fonts/JetBrainsMono-Regualr.ttf"
-          as="font"
-          crossOrigin=""
-        />
+        <link rel="icon" href="/chicano-flame.png" />
       </Head>
-      <div className="page-header mt-8">
-        <h1>Hi, I'm Hector!</h1>
-        <p className="text-xl">💻 🎧 ☕️</p>
-      </div>
-      <div className="my-10">
-        <div className="grid-gap-2 my-2">
-          <div className="mb-8">
-            <p className="text-md mb-4">
-              A <i>front-end web developer</i> focused on Html, CSS &amp;
-              JavaScript. By day I am a Front-End Web Developer. By night I am
-              working on my never ending list of side projects. If you feel like
-              following a normal person on Twitter, click here. No threads,
-              pinky promise.
-            </p>
-            <p className="text-sm">- Your Friendly Neighborhood Chicano</p>
-          </div>
-        </div>
-        <TechSlider />
-        <FeaturedProjects />
-      </div>
+      <Hero />
+
     </div>
-  );
+  )
 }
