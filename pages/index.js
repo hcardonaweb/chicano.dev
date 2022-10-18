b3cc0e1f8c2249888c88c6fb0a29bd633f77ebbe
Hector Cardona <hector@tm5150.com>
2022-10-18 11:39:08 -0700

    added meta image

--- pages/index.js
+++ pages/index.js
@@ -29,10 +29,7 @@ export default function Home() {
           property="og:description"
           content="Hi I'm Hector a Front-End Web Developer based out of San Diego, CA."
         ></meta>
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        ></meta>
+        <meta property="og:image" content="/chicano-dev-meta.png"></meta>
 
         <meta property="twitter:card" content="summary_large_image"></meta>
         <meta property="twitter:url" content="https://metatags.io/"></meta>
@@ -44,10 +41,7 @@ export default function Home() {
           property="twitter:description"
           content="Hi I'm Hector a Front-End Web Developer based out of San Diego, CA."
         ></meta>
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        ></meta>
+        <meta property="twitter:image" content="/chicano-dev-meta.png"></meta>
 
         <link rel="icon" href="/chicano-flame.png" />
       </Head>
