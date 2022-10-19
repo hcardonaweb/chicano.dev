2df8da3a13cb404ee908f83ddc8fd69b6e8b8db6
Hector Cardona <hector@tm5150.com>
2022-10-19 16:02:37 -0700

    mild reskin

--- components/Hero.js
+++ components/Hero.js
@@ -4,18 +4,15 @@ import MediaLinks from "./SocialLinks";
 const Hero = () => {
   return (
     <section className="text-white">
-      <div
-        className="mx-auto max-w-screen-2xl px-4 py-32 flex h-screen items-center"
-      >
+      <div className="mx-auto max-w-screen-2xl px-4 py-32 flex h-screen items-center">
         <div className="mx-auto max-w-3xl text-center">
-          <Heading as="h1" size="2xl" color="pinkPurple">
-            Portfolio v2.0.0
-            Coming Soon
-         </Heading>
+          <Heading as="h1" size="2xl" className="drop-shadow-lg">
+            Portfolio v2.0.0 Coming Soon
+          </Heading>
           <MediaLinks />
         </div>
       </div>
     </section>
-    )
-}
-export default Hero
+  );
+};
+export default Hero;
--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -6,11 +6,11 @@ const MediaLinks = () => {
           href="https://github.com/hcardonaweb"
           rel="noreferrer"
           target="_blank"
-          className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+          className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 drop-shadow-lg"
         >
           <span className="sr-only">GitHub</span>
           <svg
-            className="h-6 w-6"
+            className="h-8 w-8"
             fill="currentColor"
             viewBox="0 0 24 24"
             aria-hidden="true"
@@ -28,11 +28,11 @@ const MediaLinks = () => {
           href="https://www.instagram.com/chicanodev/"
           rel="noreferrer"
           target="_blank"
-          className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+          className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 drop-shadow-lg"
         >
           <span className="sr-only">Instagram</span>
           <svg
-            className="h-6 w-6"
+            className="h-8 w-8"
             fill="currentColor"
             viewBox="0 0 24 24"
             aria-hidden="true"
@@ -50,23 +50,21 @@ const MediaLinks = () => {
           href="https://twitter.com/ChicanoDev"
           rel="noreferrer"
           target="_blank"
-          className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+          className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75 drop-shadow-lg"
         >
           <span className="sr-only">Twitter</span>
           <svg
-            className="h-6 w-6"
+            className="h-8 w-8"
             fill="currentColor"
             viewBox="0 0 24 24"
             aria-hidden="true"
           >
-            <path
-              d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
-            />
+            <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
           </svg>
         </a>
       </li>
     </ul>
-  )
-}
+  );
+};
 
-export default MediaLinks
+export default MediaLinks;
