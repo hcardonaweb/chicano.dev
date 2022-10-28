977768837956b25402239a52ec8318a02c2ead34
Hector Cardona <hector@tm5150.com>
2022-10-28 11:34:20 -0700

    updated projects component

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import Nav from "./Nav";
-import Hero from "./Hero";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -8,32 +8,10 @@ const ProjectCard = () => {
           class="h-56 w-full object-cover"
         />
 
-        <div class="p-4">
-          <p class="text-xs text-gray-500">website.com</p>
+        <div class="p-4 charcoal-clear">
+          <p class="text-xs text-gray-500">chicano.dev</p>
 
-          <h3 class="text-sm text-white">
-            How to position your furniture for positivity
-          </h3>
-
-          <p class="mt-1 text-xs text-gray-500">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
-            nobis aliquid accusamus? Sint, sequi voluptas.
-          </p>
-        </div>
-      </a>
-      <a href="#" class="block overflow-hidden rounded-lg">
-        <img
-          alt="Office"
-          src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-          class="h-56 w-full object-cover"
-        />
-
-        <div class="p-4">
-          <p class="text-xs text-gray-500">website.com</p>
-
-          <h3 class="text-sm text-white">
-            How to position your furniture for positivity
-          </h3>
+          <h3 class="text-sm text-white">This website!</h3>
 
           <p class="mt-1 text-xs text-gray-500">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
