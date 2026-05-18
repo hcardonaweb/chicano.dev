6f74a9aece566f0946d3ba59ac77dd729cfd7d52
Hector Cardona <hcardonaweb@gmail.com>
2026-05-18 10:42:14 -0700

    feat
    updated about page and container styling

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col justify-between scroll-smooth px-5 max-w-[1440px] mx-auto ">
       <Nav />
-      <div className="container">
+      <div className="container w-full mx-auto">
         <main className="py-4">{children}</main>
       </div>
       <Footer />
